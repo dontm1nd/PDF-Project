e623429adfeb453f760900e54fa7e5ca532b8b63
D0ntmind <nils.best@protonmail.com>
2025-03-13 20:06:39 +0100

    init dashboard

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,9 @@
+export default function dashboard(){
+    return(
+        <div className="flex min-h-screen items-center justify-center bg-gray-100">
+            <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+                <h2 className="text-gray-700 text-center">PDF-Creator</h2>
+            </div>
+        </div>
+    )
+}
