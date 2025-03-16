233ac42fe0c399f30d2306a11c417aba8fe353ac
D0ntmind <nils.best@protonmail.com>
2025-03-16 18:18:29 +0100

    Update dashboard and add image

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,9 +1,38 @@
 export default function dashboard(){
-    return(
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-                <h2 className="text-gray-700 text-center">PDF-Creator</h2>
+    return( 
+        <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl flex flex-col min-h-screen items-center justify-center">
+            <div className="md:flex">
+                <div className="w-full p-3">
+                    <div className="relative h-48 rounded-lg border-2 border-blue-500 bg-gray-50 flex justify-center items-center shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <div className="absolute flex flex-col items-center">
+                        <img
+                            alt="File Icon"
+                            className="mb-3 w-1/2"
+                            src="public/img/jpg.png"
+                        />
+                        <span className="block text-gray-500 font-semibold"
+                            >Drag &amp; drop your files here
+                        </span>
+                        <span className="block text-gray-400 font-normal mt-1"
+                            >or click to upload
+                        </span>
+                        </div>
+
+                        <input
+                            name=""
+                            className="h-full w-full opacity-0 cursor-pointer"
+                            type="file"
+                            accept="image/jpg"
+                            //onChange={handleFileChange}
+                        />
+                    </div>
+                </div>
             </div>
+            
+            <button className="w-40 h-16 text-white font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:shadow-[#c6cbcd] hover:cursor-pointer">
+                Convert
+            </button>
+
         </div>
     )
 }
